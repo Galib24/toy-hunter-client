59111ad0b71752d85aa96655cbba2462c209596c
SQLAdmin <abuyeahia24@gmail.com>
2023-05-18 03:21:36 +0600

    added banner slider

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,83 @@
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[600px]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/8KK0qDV/dhd.png" className="w-full rounded-xl" />
+
+                <div className="absolute rounded-xl flex items-center  h-full left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl">This is about kids sections.</h2>
+                        <p>In this sections here all kind of toys we provide which enjoyable and also learnable</p>
+                        <div>
+                            <button className="btn btn-accent mr-5">Discover Here</button>
+                            <button className="btn btn-outline btn-accent">Latest Toys</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/54K7jwX/sc2.png" className="w-full rounded-xl" />
+
+                <div className="absolute rounded-xl flex items-center  h-full left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl">This is about kids sections.</h2>
+                        <p>In this sections here all kind of toys we provide which enjoyable and also learnable</p>
+                        <div>
+                            <button className="btn btn-accent mr-5">Discover Here</button>
+                            <button className="btn btn-outline btn-accent">Latest Toys</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/Tbc2Qq8/sc1.png" className="w-full rounded-xl" />
+
+                <div className="absolute rounded-xl flex items-center  h-full left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl">This is about kids sections.</h2>
+                        <p>In this sections here all kind of toys we provide which enjoyable and also learnable</p>
+                        <div>
+                            <button className="btn btn-accent mr-5">Discover Here</button>
+                            <button className="btn btn-outline btn-accent">Latest Toys</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/gWXkWXN/dsfhds.png" className="w-full rounded-xl" />
+
+                <div className="absolute rounded-xl flex items-center  h-full left-0 top-0  bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+                    <div className="text-white space-y-7 pl-12 w-1/2">
+                        <h2 className="text-6xl">This is about kids sections.</h2>
+                        <p>In this sections here all kind of toys we provide which enjoyable and also learnable</p>
+                        <div>
+                            <button className="btn btn-accent mr-5">Discover Here</button>
+                            <button className="btn btn-outline btn-accent">Latest Toys</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
