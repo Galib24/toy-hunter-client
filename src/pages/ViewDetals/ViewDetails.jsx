2bd221492dfea3040cc6229c3d28bb5f61a4dc6d
SQLAdmin <abuyeahia24@gmail.com>
2023-05-18 19:47:23 +0600

    added ui design for view details

--- src/pages/ViewDetals/ViewDetails.jsx
+++ src/pages/ViewDetals/ViewDetails.jsx
@@ -3,21 +3,29 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 
 const ViewDetails = () => {
-    const {detailsId} = useParams();
+    const { detailsId } = useParams();
     const details = useLoaderData();
     const [categoryDetails, setCategoryDetails] = useState({})
-    useEffect(()=>{
-        if(details){
+    useEffect(() => {
+        if (details) {
             const detailsData = details.find(dt => dt._id == detailsId);
             setCategoryDetails(detailsData)
         }
-    },[])
-    const {_id, title} = categoryDetails;
+    }, [])
+    const { _id, title, img, price, description, Rating } = categoryDetails;
     console.log(_id);
     return (
-        <div>
-            <h2>{_id}</h2>
-            <h1>{title}</h1>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={img} className="max-w-sm rounded-lg shadow-2xl" />
+                <div className="ml-6">
+                    <h1 className="text-5xl font-bold">{title}</h1>
+                    <p className="py-2">Price: ${price}</p>
+                    <p className="py-1">Rating: {Rating}</p>
+                    <p className="py-1">{description}</p>
+                    <button className="btn btn-primary">Check Out</button>
+                </div>
+            </div>
         </div>
     );
 };
